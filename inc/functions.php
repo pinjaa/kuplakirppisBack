938b35a092499a88247945c5bf1aedde36eff02a
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-24 14:18:05 +0200

    kirjoitusvirheen korjaus

--- inc/functions.php
+++ inc/functions.php
@@ -3,7 +3,7 @@ function openDB(): object {
     $ini = parse_ini_file("../config.ini", true);
     $host = $ini['host'];
     $database = $ini['database'];
-    $user = $ini['user'];
+    $user = $ini['username'];
     $password = $ini['password'];
     $db = new PDO('mysql:host=$host;dbname=$database;charset=utf8',$user,$password);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
