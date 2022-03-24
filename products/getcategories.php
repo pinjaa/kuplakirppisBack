11cc7908776620c32d30c4125e46177ee0c7c744
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-24 10:01:09 +0200

    lisätty tuotehaku

--- products/getcategories.php
+++ products/getcategories.php
@@ -4,6 +4,11 @@ require_once '../inc/headers.php';
 
 try {
     $db = openDB();
+    $sql = "select * from tuote";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('HTTP/1.1 200 OK');
+    print json_encode($results);
 } catch (PDOException $pdoex) {
     returnError($pdoex);
 }
