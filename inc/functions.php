708c7f272882a51458696abba87df5fce008b6d7
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-24 13:57:24 +0200

    lisää funktioita

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,30 @@
 <?php
-function openDB() {
-    $db = new PDO('mysql:host=localhost;dbname=tuote;charset=utf8','root','');
+function openDB(): object {
+    $ini = parse_ini_file("../config.ini", true);
+    $host = $ini['host'];
+    $database = $ini['database'];
+    $user = $ini['user'];
+    $password = $ini['password'];
+    $db = new PDO('mysql:host=$host;dbname=$database;charset=utf8',$user,$password);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     return $db;
 }
 
-function returnError($pdoex) {
+function selectAsJson(object $db, string $sql): void {
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('HTTP/1.1 200 OK');
+    echo json_encode($results);
+}
+
+function executeInsert(object $db,string $sql): int {
+    $query = $db->query($sql);
+    return $db->lastInsertId();
+}
+
+function returnError(PDOException $pdoex): void {
     header('HTTP/1.1 500 Internal Server Error');
     $error = array('error' => $pdoex->getMessage());
-    print json_encode($error);
+    echo json_encode($error);
+    exit;
 }
