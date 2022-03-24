bd2a0c88fe3a6d4e206cc6a10969b01940e73af3
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-24 14:18:26 +0200

    try täydennetty

--- products/getcategories.php
+++ products/getcategories.php
@@ -4,11 +4,7 @@ require_once '../inc/headers.php';
 
 try {
     $db = openDB();
-    $sql = "select * from tuote";
-    $query = $db->query($sql);
-    $results = $query->fetchAll(PDO::FETCH_ASSOC);
-    header('HTTP/1.1 200 OK');
-    print json_encode($results);
+    selectAsJson($db, 'select * from kategoria');
 } catch (PDOException $pdoex) {
     returnError($pdoex);
 }
