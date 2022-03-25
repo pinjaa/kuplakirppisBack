ecaa132585fd8302aa7f5dafa9e6ea2d937b317e
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-03-25 14:00:53 +0200

    korjattu virheet, nyt toimii

--- inc/functions.php
+++ inc/functions.php
@@ -5,7 +5,7 @@ function openDB(): object {
     $database = $ini['database'];
     $user = $ini['username'];
     $password = $ini['password'];
-    $db = new PDO('mysql:host=$host;dbname=$database;charset=utf8',$user,$password);
+    $db = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$user,$password);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     return $db;
 }
