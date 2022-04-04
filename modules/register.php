19c218c4342e9700c2920e47e3496297a4553280
paulihaarnio <pauli.haarnio@gmail.com>
2022-04-04 14:57:20 +0300

    rekisteröinti

--- modules/register.php
+++ modules/register.php
@@ -6,18 +6,17 @@ require_once "../inc/headers.php";
 //Jos parametria ei löydy, funktio palauttaa null
 $fname = filter_input(INPUT_POST, "etunimi");
 $lname = filter_input(INPUT_POST, "sukunimi");
-$uname=filter_input(INPUT_POST, "tunnus");
 $pword=filter_input(INPUT_POST, "salasana");
 $email=filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
 
 //Tarkistetaan onko muttujia asetettu
-if( !isset($fname) || !isset($lname) || !isset($uname)|| !isset($pword)|| !isset($email)){
+if( !isset($fname) || !isset($lname) || !isset($pword)|| !isset($email)){
     echo "Parametreja puuttui!! Ei voida lisätä henkilöä";
     exit;
 }
 
 //Tarkistetaan, ettei tyhjiä arvoja muuttujissa
-if( empty($fname) || empty($lname) || empty($uname)|| empty($pword)|| empty($email)){
+if( empty($fname) || empty($lname) || empty($pword)|| empty($email)){
     echo "Et voi asettaa tyhjiä arvoja!!";
     exit;
 }
@@ -25,22 +24,23 @@ if( empty($fname) || empty($lname) || empty($uname)|| empty($pword)|| empty($ema
 try{
     $db = openDB();
     //Suoritetaan parametrien lisääminen tietokantaan.
-    $sql = "INSERT INTO as_tili (etunimi, sukunimi, tunnus, salasana, email) VALUES (?,?,?,?,?)";
+    $sql = "INSERT INTO as_tili (etunimi, sukunimi, salasana, email) VALUES (?,?,?,?)";
     $statement = $db->prepare($sql);
     $statement->bindParam(1, $fname);
     $statement->bindParam(2, $lname);
-    $statement->bindParam(3, $uname);
+    
     
     $hashpw= password_hash($pword, PASSWORD_DEFAULT);
     password_verify($pword, $hashpw);
-    $statement->bindParam(4, $hashpw);
-    $statement->bindParam(5,$email);
+    $statement->bindParam(3, $hashpw);
+    $statement->bindParam(4,$email);
     $statement->execute();
 
-    echo "Tervetuloa ".$fname." ".$lname.". Sinut on lisätty tietokantaan käyttäjänimellä "."$uname"; 
+    echo "Tervetuloa ".$fname." ".$lname.". Sinut on lisätty tietokantaan sähköpostilla "."$email"; 
 }catch(PDOException $e){
     echo "Käyttäjää ei voitu lisätä<br>";
     echo $e->getMessage();
+    header("Location:http://localhost:3000/");
 }
 
 ?>
