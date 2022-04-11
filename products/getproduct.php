d407a900d81ebe40f58e0164fefbe53793c3b0b3
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-11 14:05:10 +0300

    muutoksia

--- products/getproduct.php
+++ products/getproduct.php
@@ -6,14 +6,17 @@ $url = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
 $parameters = explode('/',$url);
 $category_id = $parameters[1];
 //miten saa valittua useamman merkin koska id voi olla enemmän kuin 1 numero
-$product_id = $parameters[3];
+/* $lastElement = count($parameters) - 1;
+$secondLastElement = count($parameters) - 2;
+$product_id = $lastElement + $secondLastElement; */
+$product_id = array_slice($parameters,2,3);
 
 try {
     $db = openDB();
     
-    $sql = "select * from kategoria where ktg_nro = $category_id";
+    $sql = "select ktg_nro from kategoria where ktg_nro = $category_id";
     $query = $db->query($sql);
-    $category = $query->fetch(PDO::FETCH_ASSOC);
+    $category = $query->fetch(PDO::FETCH_ASSOC); 
 
     $sql = "select * from tuote where id = $product_id";
     $query = $db->query($sql);
@@ -21,7 +24,7 @@ try {
 
     header('HTTP/1.1 200 OK');
     echo json_encode(array(
-        "category" => $category['ktg_nimi'],
+        "category" => $category['ktg_nro'],
         "product" => $product
     )); 
 }
