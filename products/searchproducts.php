f15dfa961e2d4bd8a513ebdf06c9c00cee8aaddf
rikulis <remesrikua@gmail.com>
2022-04-13 13:44:49 +0300

    Searchbar toimii category id:lä

--- products/searchproducts.php
+++ products/searchproducts.php
@@ -4,11 +4,12 @@ require_once '../inc/headers.php';
 
 $uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
 $parameters = explode('/',$uri);
-$phrase = $parameters[1];
+
+$searchPhrase = $parameters[1];
 
 try {
     $db = openDb();
-    $sql = "select * from tuote where name like '%$phrase%'";
+    $sql = "select * from tuote where tuotenimi like '%$searchPhrase%'";
     selectAsJson($db,$sql);
 } 
 catch (PDOException $pdoex) {
