6e343cede93743ee71f13622c10bcbe024a250aa
booennm <booennm@gmail.com>
2022-04-13 13:04:39 +0300

    jsonin postaus toimii

--- modules/order.php
+++ modules/order.php
@@ -2,15 +2,18 @@
 require_once "../inc/functions.php";
 require_once "../inc/headers.php";
 
+$input = file_get_contents('php://input');
+$input = json_decode($input);
+
 //Filtteroidaan POST-inputit
 //orderFormiin syötetyt tiedot
-$fname = filter_input(INPUT_POST, "etunimi");
-$lname = filter_input(INPUT_POST, "sukunimi");
-$address=filter_input(INPUT_POST, "osoite");
-$zip=filter_input(INPUT_POST, "postinro");
-$city=filter_input(INPUT_POST, "postitmp");
-$email=filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
-$phone=filter_input(INPUT_POST, "puhelinnro");
+$fname = filter_var($input->etunimi, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$lname = filter_var($input->sukunimi, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$address=filter_var($input->osoite, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$zip=filter_var($input->postinro, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$city=filter_var($input->postitmp, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$email=filter_var($input->email, FILTER_SANITIZE_EMAIL);
+$phone=filter_var($input->puhelinnro, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 /* //Tarkistetaan onko muuttujia asetettu
 if( !isset($fname) || !isset($lname) || !isset($address) || !isset($zip) || !isset($city) || !isset($email) || !isset($phone)){
@@ -30,25 +33,27 @@ try{
     //Suoritetaan parametrien lisääminen tietokantaan.
     //$sql = "INSERT INTO tilaus (asiakasnro, tilauspvm) VALUES ((SELECT asiakasnro FROM as_tili where email=:email), curdate())"; jos kirjautuneena
 
-    $sql = "INSERT INTO as_tili (etunimi, sukunimi, email, osoite, postinro, postitmp, puhelinnro) VALUES (?,?,?,?,?,?,?);";
+    $sql = "INSERT INTO as_tili (etunimi, sukunimi, email, osoite, postinro, postitmp, puhelinnro) VALUES ('" .
+        filter_var($fname, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($lname, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($email, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($address, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($zip, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($city, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . "','" .
+        filter_var($phone, FILTER_SANITIZE_FULL_SPECIAL_CHARS)
+    . "')";
 
-    $statement = $db->prepare($sql);
-    $statement->bindParam(1, $fname, PDO::PARAM_STR);
-    $statement->bindParam(2, $lname, PDO::PARAM_STR);
-    $statement->bindParam(3, $email, PDO::PARAM_STR);
-    $statement->bindParam(4, $address, PDO::PARAM_STR);
-    $statement->bindParam(5, $zip, PDO::PARAM_STR);
-    $statement->bindParam(6, $city, PDO::PARAM_STR);
-    $statement->bindParam(7, $phone, PDO::PARAM_STR);
-    $statement->execute();
+    $customer_id = executeInsert($db, $sql);
 
-    $sql = "INSERT INTO tilaus (asiakasnro, tilauspvm) VALUES ((SELECT asiakasnro FROM as_tili where email=:email), curdate())";
+    $sql = "INSERT INTO tilaus (asiakasnro, tilauspvm) VALUES ($customer_id, curdate())";
 
-    $statement = $db->prepare($sql);
-    $statement->bindParam(':email', $email, PDO::PARAM_STR);
-    $statement->execute();
+    $order_id = executeInsert($db, $sql);
 
     $db->commit();
+
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $customer_id);
+    echo json_encode($data);
 
     echo "Henkilön ".$fname." ".$lname." tilaus lisätty tietokantaan."; 
 }catch(PDOException $e){
