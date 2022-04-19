1e5fa0aef4f9cca0bddadd8f6e08ef7b0d88db08
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-19 14:49:05 +0300

    muokattu requiret

--- products/addProduct.php
+++ products/addProduct.php
@@ -1,6 +1,6 @@
 <?php
-require_once './inc/functions.php';
-require_once './inc/headers.php';
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
 
 $input=json_decode(file_get_contents('php://input'));
 $name=filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
