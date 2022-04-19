e647f12d780b6aaf13224765e19fce7635161c32
paulihaarnio <pauli.haarnio@gmail.com>
2022-04-19 11:05:31 +0300

    add php

---
+++ products/addCategory.php
@@ -0,0 +1,17 @@
+<?php
+require_once './inc/functions.php';
+require_once './inc/headers.php';
+
+$input= json_decode(file_get_contents('php://input'));
+$name=filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+try{
+    $db=openDB();
+    $sql="insert into kategoria (name) values ('$name')";
+    executeInsert($db, $sql);
+    $data=array('id' =>$db->lastInsertID(),'name'=>$name);
+    print json_encode($data);
+}catch(PDOException $pdoex){
+    returnError($pdoex);
+}
+  
