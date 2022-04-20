e9a0933a7866f6e4d947e01b6ede0282519cfb78
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-20 12:00:54 +0300

    korjattu insert into lause

--- products/addProduct.php
+++ products/addProduct.php
@@ -11,7 +11,7 @@ $categoryID=filter_var($input->categoryID,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try{
     $db=openDB();
-    $sql="insert into tuote (name,price,image,description,categoryID) values('$name','$price','placeholder.png','$description','$categoryID')";
+    $sql="insert into tuote (tuotenimi,hinta,image,kuvaus,ktg_nro) values('$name','$price','placeholder.png','$description','$categoryID')";
     executeInsert($db,$sql);
     $data=array('id'=>$db->lastInsertID(),'name'=>$name,'price'=>$name,'image'=>'placeholder.png');
     print json_encode($data);
