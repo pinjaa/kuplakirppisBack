425d242bed849ca4cdc95a8edc6d9b47047f2bbe
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-22 09:49:40 +0300

    logout lisätty

--- modules/login.php
+++ modules/login.php
@@ -3,61 +3,73 @@ require_once "../inc/functions.php";
 require_once "../inc/headers.php";
 
 
- //Käynnistetään sessio, johon talletetaan käyttäjä, jos kirjautuminen onnistuu
- session_start();
-/*  $fname = filter_input(INPUT_POST, "etunimi");
- $lname = filter_input(INPUT_POST, "sukunimi"); */
- $email = filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
- $pword = filter_input(INPUT_POST, "salasana");
-
-
- /*//Tarkistetaan onko muttujia asetettu
- if( !isset($email) || !isset($pword) ){
-     echo "Parametreja puuttui!! Ei voida kirjautua.";
-     exit;
- }
+function login() {
+     //Käynnistetään sessio, johon talletetaan käyttäjä, jos kirjautuminen onnistuu
+    session_start();
+    /*  $fname = filter_input(INPUT_POST, "etunimi");
+  $lname = filter_input(INPUT_POST, "sukunimi"); */
+  $email = filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
+  $pword = filter_input(INPUT_POST, "salasana");
  
-
- //Tarkistetaan, ettei tyhjiä arvoja muuttujissa
- if( empty($email) || empty($pword) ){
-     echo "Et voi asettaa tyhjiä arvoja!!";
-     exit;
- }*/
-
- try{
-    $db = openDB();
-     //Haetaan käyttäjä annetulla sähköpostiosoitteella
-     $sql = "SELECT * FROM kayttaja_tili WHERE email=?";
-     $statement = $db->prepare($sql);
-     $statement->bindParam(1, $email);
-     $statement->execute();
-
-     if($statement->rowCount() <=0){
-         echo "Käyttäjää ei löydy!!";
-         exit;
-     }
  
-     $row = $statement->fetch();
+  /*//Tarkistetaan onko muttujia asetettu
+  if( !isset($email) || !isset($pword) ){
+      echo "Parametreja puuttui!! Ei voida kirjautua.";
+      exit;
+  }
+  
+ 
+  //Tarkistetaan, ettei tyhjiä arvoja muuttujissa
+  if( empty($email) || empty($pword) ){
+      echo "Et voi asettaa tyhjiä arvoja!!";
+      exit;
+  }*/
+ 
+  try{
+     $db = openDB();
+      //Haetaan käyttäjä annetulla sähköpostiosoitteella
+      $sql = "SELECT * FROM kayttaja_tili WHERE email=?";
+      $statement = $db->prepare($sql);
+      $statement->bindParam(1, $email);
+      $statement->execute();
+ 
+      if($statement->rowCount() <=0){
+          echo "Käyttäjää ei löydy!!";
+          exit;
+      }
+  
+      $row = $statement->fetch();
+ 
+      //Tarkistetaan käyttäjän antama salasana tietokannan salasanaa vasten
+      if(!password_verify($pword, $row["salasana"] )){
+          echo "Väärä salasana!!";
+          exit;
+      }
+ 
+      //Jos käyttäjä tunnistettu, talletetaan käyttäjän tiedot sessioon
+      $_SESSION["email"] = $email;
+     /*  $_SESSION["fname"] = $row["etunimi"];
+      $_SESSION["lname"] = $row["sukunimi"]; */
+ 
+      echo "Tervetuloa. Kirjautuminen onnistui "."$email"; 
+ 
+  }catch(PDOException $e){
+      echo "Kirjautuminen ei onnistunut<br>";
+      echo $e->getMessage();
+          //Ohjataan takaisin etusivulle
+          header("Location:http://localhost:3000/");
+  }
+}
 
-     //Tarkistetaan käyttäjän antama salasana tietokannan salasanaa vasten
-     if(!password_verify($pword, $row["salasana"] )){
-         echo "Väärä salasana!!";
-         exit;
-     }
-
-     //Jos käyttäjä tunnistettu, talletetaan käyttäjän tiedot sessioon
-     $_SESSION["email"] = $email;
-    /*  $_SESSION["fname"] = $row["etunimi"];
-     $_SESSION["lname"] = $row["sukunimi"]; */
-
-     echo "Tervetuloa. Kirjautuminen onnistui "."$email"; 
-
- }catch(PDOException $e){
-     echo "Kirjautuminen ei onnistunut<br>";
-     echo $e->getMessage();
-         //Ohjataan takaisin etusivulle
-         header("Location:http://localhost:3000/");
- }
+function logout() {
+    //Tyhjennetään ja tuhotaan nykyinen sessio.
+    try{
+        session_unset();
+        session_destroy();
+    }catch(Exception $e){
+        throw $e;
+    }
+}
 ?>
 
 
