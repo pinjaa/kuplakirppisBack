c086e11348a62dc43b54daee67ba1c43d05fd368
rikulis <remesrikua@gmail.com>
2022-04-23 13:22:25 +0300

    EI TOIMI VIELÄ'

--- products/customer.php
+++ products/customer.php
@@ -17,9 +17,6 @@ $categoryID=filter_var($input->categoryID,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $text = filter_input(INPUT_POST,'test',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 try{
     $db=openDB();
-    $sql="insert into tuote (tuotenimi,hinta,kuvaus,ktg_nro) values('$name',$price,'$description',$categoryID)";
-   
-    $data=array('id'=>$db->lastInsertID(),'name' => $name,'price' => $price, 'kuvaus' => $description, 'ktg_nro' => $categoryID);
     if (isset($_FILES['file'])) {
 
         if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
@@ -31,7 +28,13 @@ try{
     
                 if (move_uploaded_file($_FILES['file']['tmp_name'],$path)) {
                     $dataS = array('filename' => $filename, 'type' => $type,'text' => $text);
+                    
+                    $sql="insert into tuote (tuotenimi,hinta,image,kuvaus,ktg_nro) values('$name',$price,'$text','$description',$categoryID)";
+                   
+                    $data=array('id'=>$db->lastInsertID(),'name' => $name,'price' => $price,'image' => $text, 'kuvaus' => $description, 'ktg_nro' => $categoryID);
                     echo json_encode($dataS);
+                    executeInsert($db,$sql);
+                    print json_encode($data);
                 } else {
                     echo 'Error saving file to upload';
                 }
@@ -44,8 +47,8 @@ try{
      } else  {
          echo 'File was not submitted.';
      } 
-     executeInsert($db,$sql);
-    print json_encode($data);
+
+  
 }catch(PDOException $pdoex){
     returnError($pdoex);
 }
