547d78817b866a41f50c623ff450e73f19b88904
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-23 13:11:53 +0300

    pari muutosta

--- modules/register.php
+++ modules/register.php
@@ -8,40 +8,59 @@ $fname = filter_input(INPUT_POST, "etunimi");
 $lname = filter_input(INPUT_POST, "sukunimi");
 $pword=filter_input(INPUT_POST, "salasana");
 $email=filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
-/*
-//Tarkistetaan onko muttujia asetettu
-if( !isset($fname) || !isset($lname) || !isset($pword)|| !isset($email)){
-    echo "Parametreja puuttui!! Ei voida lisätä henkilöä";
-    exit;
-}
+$user = filter_input(INPUT_POST,"user_type");
 
-//Tarkistetaan, ettei tyhjiä arvoja muuttujissa
-if( empty($fname) || empty($lname) || empty($pword)|| empty($email)){
-    echo "Et voi asettaa tyhjiä arvoja!!";
-    exit;
-}*/
+
 
 try{
     $db = openDB();
-    //Suoritetaan parametrien lisääminen tietokantaan.
-    $sql = "INSERT INTO kayttaja_tili (etunimi, sukunimi, salasana, email) VALUES (?,?,?,?)";
-    $statement = $db->prepare($sql);
-    $statement->bindParam(1, $fname);
-    $statement->bindParam(2, $lname);
-    
-    
-    $hashpw= password_hash($pword, PASSWORD_DEFAULT);
-    password_verify($pword, $hashpw);
-    $statement->bindParam(3, $hashpw);
-    $statement->bindParam(4,$email);
-    $statement->execute();
+    if($user == null) {
+        $user = 'E';
+        //Suoritetaan parametrien lisääminen tietokantaan.
+        $sql = "INSERT INTO kayttaja_tili (etunimi, sukunimi, salasana, email, admin_oikeus) VALUES (?,?,?,?,?)";
+        $statement = $db->prepare($sql);
+        $statement->bindParam(1, $fname);
+        $statement->bindParam(2, $lname);
 
-    echo json_encode("Tervetuloa ".$fname." ".$lname.". Sinut on lisätty tietokantaan sähköpostilla "."$email");
-    header("Location:http://localhost:3000/pages/Register");
+        $hashpw= password_hash($pword, PASSWORD_DEFAULT);
+        password_verify($pword, $hashpw);
+        $statement->bindParam(3, $hashpw);
+        $statement->bindParam(4,$email);
+        $statement->bindParam(5,$user);
+        $statement->execute();
+        
+        $string = "Tervetuloa sinut on lisätty tietokantaan sähköpostilla ";
+
+        $parts = [];
+        $tok = strtok($string, " ");
+        while ($tok !== false) {
+            $parts[] = $tok;
+            $tok = strtok(" ");
+        }
+        echo json_encode($parts),"\n";
+        header("Location: http://localhost:3000/pages/Register");
+    } else {
+        //Suoritetaan parametrien lisääminen tietokantaan.
+        $sql = "INSERT INTO kayttaja_tili (etunimi, sukunimi, salasana, email, admin_oikeus) VALUES (?,?,?,?,?)";
+        $statement = $db->prepare($sql);
+        $statement->bindParam(1, $fname);
+        $statement->bindParam(2, $lname);
+        
+        $hashpw= password_hash($pword, PASSWORD_DEFAULT);
+        password_verify($pword, $hashpw);
+        $statement->bindParam(3, $hashpw);
+        $statement->bindParam(4,$email);
+        $statement->bindParam(5,$user);
+        $statement->execute();
+
+       // print json_encode("Tervetuloa ".$fname." ".$lname.". Sinut on lisätty tietokantaan sähköpostilla "."$email");
+        header("Location: http://localhost:3000/pages/Register");
+    }
+    
 }catch(PDOException $e){
   //echo "Käyttäjää ei voitu lisätä<br>";
-   // echo $e->getMessage();
-    header("Location:http://localhost:3000/");
+    echo $e->getMessage();
+    //header("Location: http://localhost:3000/");
 }
 
 ?>
