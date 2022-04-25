b462f9aeb8a7ed57c97f29caadd486b56bbc75e2
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-25 14:52:39 +0300

    muokattu sql

--- products/updateProducts.php
+++ products/updateProducts.php
@@ -2,18 +2,21 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
+$url = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
+$parameters = explode('/',$url);
+$category_id = $parameters[1];
+$product_id = $parameters[2];
+
 $input=json_decode(file_get_contents('php://input'));
 $name=filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $price=filter_var($input->price,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$image=filter_var($input->image,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $description=filter_var($input->description,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$categoryID=filter_var($input->categoryID,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try{
     $db=openDB();
-    $sql="alter table tuote (tuotenimi,hinta,image,kuvaus,ktg_nro) values('$name',$price, '$image','$description',$categoryID)";
+    $sql="update table tuote SET tuotenimi = '$name', hinta = $price, kuvaus = '$description' WHERE id = $product_id";
     executeInsert($db,$sql);
-    $data=array('id'=>$db->lastInsertID(),'tuotenimi' => $name,'hinta' => $price, 'image' => $image, 'kuvaus' => $description, 'ktg_nro' => $categoryID);
+    $data=array('id'=>$product_id,'tuotenimi' => $name,'hinta' => $price, 'kuvaus' => $description);
     print json_encode($data);
 }catch(PDOException $pdoex){
     returnError($pdoex);
