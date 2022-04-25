3dd166fd0256ec4326c0c4c3d160fc432d05c552
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-04-25 12:25:45 +0300

    lisätty kuvan lisäys sqlään

--- products/addProduct.php
+++ products/addProduct.php
@@ -5,15 +5,15 @@ require_once '../inc/headers.php';
 $input=json_decode(file_get_contents('php://input'));
 $name=filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $price=filter_var($input->price,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-//$image=filter_var($input->image,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$image=filter_var($input->image,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $description=filter_var($input->description,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $categoryID=filter_var($input->categoryID,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try{
     $db=openDB();
-    $sql="insert into tuote (tuotenimi,hinta,kuvaus,ktg_nro) values('$name',$price,'$description',$categoryID)";
+    $sql="insert into tuote (tuotenimi,hinta,image,kuvaus,ktg_nro) values('$name',$price, '$image','$description',$categoryID)";
     executeInsert($db,$sql);
-    $data=array('id'=>$db->lastInsertID(),'tuotenimi' => $name,'hinta' => $price, 'kuvaus' => $description, 'ktg_nro' => $categoryID);
+    $data=array('id'=>$db->lastInsertID(),'tuotenimi' => $name,'hinta' => $price, 'image' => $image, 'kuvaus' => $description, 'ktg_nro' => $categoryID);
     print json_encode($data);
 }catch(PDOException $pdoex){
     returnError($pdoex);
