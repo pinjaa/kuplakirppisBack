07a852aa7450d351d4c66d67d9c7538e0a84a6c2
booennm <booennm@gmail.com>
2022-04-25 12:25:05 +0300

    register toimii paremmin

--- modules/register.php
+++ modules/register.php
@@ -2,15 +2,16 @@
 require_once "../inc/functions.php";
 require_once "../inc/headers.php";
 
+$input = file_get_contents('php://input');
+$input = json_decode($input);
+
 //Filtteroidaan POST-inputit (ei käytetä string-filtteriä, koska deprekoitunut)
 //Jos parametria ei löydy, funktio palauttaa null
-$fname = filter_input(INPUT_POST, "etunimi");
-$lname = filter_input(INPUT_POST, "sukunimi");
-$pword=filter_input(INPUT_POST, "salasana");
-$email=filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
-$user = filter_input(INPUT_POST,"user_type");
-
-
+$fname = filter_var($input->fname, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$lname = filter_var($input->lname, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$pword=filter_var($input->pword, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$email=filter_var($input->email, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$user = filter_var($input->user, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try{
     $db = openDB();
@@ -19,14 +20,14 @@ try{
         //Suoritetaan parametrien lisääminen tietokantaan.
         $sql = "INSERT INTO kayttaja_tili (etunimi, sukunimi, salasana, email, admin_oikeus) VALUES (?,?,?,?,?)";
         $statement = $db->prepare($sql);
-        $statement->bindParam(1, $fname);
-        $statement->bindParam(2, $lname);
-
+        $statement->bindParam(1, $fname, PDO::PARAM_STR);
+        $statement->bindParam(2, $lname, PDO::PARAM_STR);
+        
         $hashpw= password_hash($pword, PASSWORD_DEFAULT);
         password_verify($pword, $hashpw);
-        $statement->bindParam(3, $hashpw);
-        $statement->bindParam(4,$email);
-        $statement->bindParam(5,$user);
+        $statement->bindParam(3, $hashpw, PDO::PARAM_STR);
+        $statement->bindParam(4,$email, PDO::PARAM_STR);
+        $statement->bindParam(5,$user, PDO::PARAM_STR);
         $statement->execute();
         
         $string = "Tervetuloa sinut on lisätty tietokantaan sähköpostilla ";
@@ -43,14 +44,14 @@ try{
         //Suoritetaan parametrien lisääminen tietokantaan.
         $sql = "INSERT INTO kayttaja_tili (etunimi, sukunimi, salasana, email, admin_oikeus) VALUES (?,?,?,?,?)";
         $statement = $db->prepare($sql);
-        $statement->bindParam(1, $fname);
-        $statement->bindParam(2, $lname);
+        $statement->bindParam(1, $fname, PDO::PARAM_STR);
+        $statement->bindParam(2, $lname, PDO::PARAM_STR);
         
         $hashpw= password_hash($pword, PASSWORD_DEFAULT);
         password_verify($pword, $hashpw);
-        $statement->bindParam(3, $hashpw);
-        $statement->bindParam(4,$email);
-        $statement->bindParam(5,$user);
+        $statement->bindParam(3, $hashpw, PDO::PARAM_STR);
+        $statement->bindParam(4,$email, PDO::PARAM_STR);
+        $statement->bindParam(5,$user, PDO::PARAM_STR);
         $statement->execute();
 
        // print json_encode("Tervetuloa ".$fname." ".$lname.". Sinut on lisätty tietokantaan sähköpostilla "."$email");
