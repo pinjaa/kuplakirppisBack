583bca8b5520cbc7b5ce8c1b3ceed3881b96c71a
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-02 14:18:29 +0300

    lisätty alert

--- products/updateProducts.php
+++ products/updateProducts.php
@@ -11,12 +11,13 @@ $input=json_decode(file_get_contents('php://input'));
 $name=filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $price=filter_var($input->price,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $description=filter_var($input->description,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$alert = "Tuotteen tiedot päivitetty";
 
 try{
     $db=openDB();
-    $sql="update tuote SET tuotenimi = COALESCE(NULLIF('$name', ''), tuotenimi), hinta = COALESCE(NULLIF($price, ''), hinta), kuvaus = COALESCE(NULLIF('$description', ''), kuvaus) WHERE id = $product_id";
+    $sql="UPDATE tuote SET tuotenimi = COALESCE(NULLIF('$name', NULL), tuotenimi), hinta = COALESCE(NULLIF($price, NULL), hinta), kuvaus = COALESCE(NULLIF('$description', NULL), kuvaus) WHERE id = $product_id";
     executeInsert($db,$sql);
-    $data=array('id'=>$product_id,'tuotenimi' => $name,'hinta' => $price, 'kuvaus' => $description);
+    $data=array('id'=>$product_id,'tuotenimi' => $name,'hinta' => $price, 'kuvaus' => $description, 'alert' => $alert);
     print json_encode($data);
 }catch(PDOException $pdoex){
     returnError($pdoex);
