1e43f7345fd70220da7582bc6614516ff7c9da96
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-03 09:22:31 +0300

    korjattu price / hinta

--- products/updateProducts.php
+++ products/updateProducts.php
@@ -9,13 +9,13 @@ $product_id = $parameters[2];
 
 $input=json_decode(file_get_contents('php://input'));
 $name=filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$price=filter_var($input->price,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$price=filter_var($input->price,FILTER_SANITIZE_NUMBER_FLOAT);
 $description=filter_var($input->description,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $alert = "Tuotteen tiedot päivitetty";
 
 try{
     $db=openDB();
-    $sql="UPDATE tuote SET tuotenimi = COALESCE(NULLIF('$name', NULL), tuotenimi), hinta = COALESCE(NULLIF($price, NULL), hinta), kuvaus = COALESCE(NULLIF('$description', NULL), kuvaus) WHERE id = $product_id";
+    $sql="UPDATE tuote SET tuotenimi = COALESCE(NULLIF('$name', ''), tuotenimi), hinta = COALESCE(NULLIF($price, 0), hinta), kuvaus = COALESCE(NULLIF('$description', ''), kuvaus) WHERE id = $product_id";
     executeInsert($db,$sql);
     $data=array('id'=>$product_id,'tuotenimi' => $name,'hinta' => $price, 'kuvaus' => $description, 'alert' => $alert);
     print json_encode($data);
