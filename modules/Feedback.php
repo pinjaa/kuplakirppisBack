5bd5da73a469064f9118353653c68011757ce2cb
pinjaa <91983754+pinjaa@users.noreply.github.com>
2022-05-05 10:21:16 +0300

    lisätty msg fronttia varten

--- modules/Feedback.php
+++ modules/Feedback.php
@@ -3,34 +3,15 @@ require_once "../inc/functions.php";
 require_once "../inc/headers.php";
 
 //Filtteroidaan POST-inputit
-//orderFormiin syötetyt tiedot
 $fname = filter_input(INPUT_POST, "etunimi");
 $lname = filter_input(INPUT_POST, "sukunimi");
 $email=filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
 $phone=filter_input(INPUT_POST, "puhelinnro");
 
-/* //Tarkistetaan onko muuttujia asetettu
-if( !isset($fname) || !isset($lname) || !isset($address) || !isset($zip) || !isset($city) || !isset($email) || !isset($phone)){
-    echo "Tietoja puuttui, tilaus ei onnistunut";
-    exit;
-} */
-
-//Tarkistetaan, ettei tyhjiä arvoja muuttujissa
-if( empty($fname) || empty($lname) || empty($email) || empty($phone)){
-    echo "Et voi asettaa tyhjiä arvoja!!";
-    exit;
-}
-
 try{
     $db = openDB();
-    //Suoritetaan parametrien lisääminen tietokantaan.
-  //  $sql = "INSERT INTO tilaus (asiakasnro, tilauspvm) VALUES ((SELECT id FROM kayttaja_tili where email=:email), curdate())";
-
-   // $statement = $db->prepare($sql);
-   /*  $statement->bindParam(':email', $email, PDO::PARAM_STR);
-    $statement->execute(); */
-
-    echo "Palaute lähetetty"; 
+    $msg = "Palaute lähetetty";
+    echo json_encode($msg) ;
 }catch(PDOException $e){
     echo "Palautetta ei voitu lähettää<br>";
     echo $e->getMessage();
